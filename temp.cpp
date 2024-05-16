dada8813561b9b788db297449d04077cec01ccb0
RUPESH0004 <rj9082356865@gmail.com>
2024-05-16 19:42:14 +0530

    creating the tempreture class for ferahanise to celciues using the overloadig function

---
+++ temp.cpp
@@ -0,0 +1,44 @@
+// Creating the class name temperature
+// where we converting the temperature in to celcius to ferhnite and ferhnite to celcius
+// having the oveerloading funvtion name settemp
+// settemp(float)
+// sertemp(float,char)
+#include <iostream>
+using namespace std;
+class Temperature
+{
+    float ftmp, ctmp;
+
+    public:
+    void SetTemp(float t)
+    {
+        ftmp = (t * 1.8) + 32;
+        ctmp = t;
+    }
+    void SerTemp(float t, char s)
+    {
+        if(s=='f' || s=='F')
+       { ctmp = (t - 32) * 1.8;
+        ftmp = t;}
+        else if (s=='c' || s=='C')
+        {
+        ftmp = (t * 1.8) + 32;
+        ctmp = t;
+        }
+        else 
+        {
+            cout << "Invalide paramerter"<<endl;
+        }
+    }
+    string  print ()
+    {
+        string s;
+        return s="the temp in farhint is "+ to_string (ftmp)+"temp in celcius is "+ to_string (ctmp); 
+    }
+};
+int main()
+{
+    Temperature p;
+    p . SetTemp(10);
+
+}
